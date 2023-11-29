10e5b688d63cb95c60989be2f44e08bdf4f9ac0a
Abdullah Habberrih <abdul2013ly@gmail.com>
2023-11-29 08:27:32 +0000

    chore: remove dist folder

--- dist/common/prisma-config-service.d.ts
+++
@@ -1,6 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-import { PrismaOptionsFactory, PrismaServiceOptions } from 'nestjs-prisma';
-export declare class PrismaConfigService implements PrismaOptionsFactory {
-    constructor(configService: ConfigService);
-    createPrismaOptions(): PrismaServiceOptions | Promise<PrismaServiceOptions>;
-}
