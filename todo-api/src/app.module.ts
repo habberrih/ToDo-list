1d8d1687270079c462771cfc9fa334b4b7e6b557
Abdullah Habberrih <abdul2013ly@gmail.com>
2023-11-29 07:55:58 +0000

    refactor; add modules

--- todo-api/src/app.module.ts
+++ todo-api/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PrismaModule } from 'nestjs-prisma';
+import { PrismaConfigService } from './common/prisma-config-service';
 
 @Module({
-  imports: [],
+  imports: [TodoModule, ConfigModule.forRoot({isGlobal: true}),
+    PrismaModule.forRootAsync({
+      isGlobal: true,
+      useClass: PrismaConfigService,
+    }), 
+
+],
   controllers: [AppController],
   providers: [AppService],
 })
