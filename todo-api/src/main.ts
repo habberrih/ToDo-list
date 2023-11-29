8fbf6ec8f82c8fbe4353b7a82039c8c7617ba1d1
Abdullah Habberrih <abdul2013ly@gmail.com>
2023-11-29 07:57:05 +0000

    refactor: setup swagger, and change app port

--- todo-api/src/main.ts
+++ todo-api/src/main.ts
@@ -1,8 +1,37 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI
+  });
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidUnknownValues: true,
+    transform: true
+  }));
+
+  app.enableCors({
+    origin: '*',
+  });
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
+  const options = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('API docs (ToDo)')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(5000);
 }
 bootstrap();
