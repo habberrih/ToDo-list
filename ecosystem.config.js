e8ca547c434804528bf6efa7f079e10be63eaac4
habberrih <abdul2013ly@gmail.com>
2023-11-30 12:16:48 +0200

    refactor: update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,34 @@
 module.exports = {
     apps: [
-        {
-            name: 'Todo API',
-            script: 'dist/main.js',
-            args: 'start',
-            watch: true,
-            env_local: {
-                APP_ENV: 'local' // APP_ENV=local
-            },
-            env_development: {
-                APP_ENV: 'dev' // APP_ENV=dev
-            },
-            env_production: {
-                APP_ENV: 'prod' // APP_ENV=prod
-            }
-        }
-    ]
-} 
+      {
+        name: 'myapp-api',
+        script: 'npm run start:dev',
+        time: true,
+        instances: 1,
+        autorestart: true,
+        max_restarts: 50,
+        watch: false,
+        max_memory_restart: '1G',
+        env: {
+          PORT: process.env.PORT || 5000,
+          DATABASE_URL: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@localhost:5432/${process.env.POSTGRES_DB}?schema=public`
+        },
+      },
+    ],
+    deploy: {
+      production: {
+        user: process.env.USER,
+        host: process.env.HOST,
+        key: 'deploy.key',
+        ref: 'origin/master',
+        repo: 'https://github.com/habberrih/ToDo-list',
+        path: process.env.PATH,
+        'post-deploy':
+          'sudo docker compose up - d && npm install && npm build && pm2 reload ecosystem.config.js --env production && pm2 save',
+        env: {
+          NODE_ENV: 'production',
+          DATABASE_URL: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@localhost:5432/${process.env.POSTGRES_DB}?schema=public`
+        },
+      },
+    },
+  }
